600d3ab2748c12d410f306050c1ad4c5008a747c
Old-Tanks <122164367+Old-Tanks@users.noreply.github.com>
2024-09-28 16:35:00 +0300

    test gt

---
+++ gtanks/resources/library/3l/pack.cmd
@@ -0,0 +1,6 @@
+call maketara com_build com_build.tara
+call maketara passage passage.tara
+call maketara defense defense.tara
+call maketara fence fence.tara
+call maketara terrain terrain.tara
+call maketara exterior exterior.tara
