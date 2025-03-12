cbb855476187201195787f6dfe4f08b6469083fe
Old-Tanks <122164367+Old-Tanks@users.noreply.github.com>
2025-03-12 12:48:19 +0300

    test proplibs

--- gtanks/resources/library/3l/pack.cmd
+++
@@ -1,6 +0,0 @@
-call maketara com_build com_build.tara
-call maketara passage passage.tara
-call maketara defense defense.tara
-call maketara fence fence.tara
-call maketara terrain terrain.tara
-call maketara exterior exterior.tara
